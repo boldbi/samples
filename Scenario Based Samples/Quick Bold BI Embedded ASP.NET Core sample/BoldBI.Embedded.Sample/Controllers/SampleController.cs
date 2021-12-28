7ffada3670c83596635c70e2c76f03296c7190de
Dheepa Kumar <dheepa.kumar@syncfusion.com>
2021-12-28 11:39:18 +0530

    UMP-18483_selected_dashboard_render_sample_committed

---
+++ Scenario Based Samples/Quick Bold BI Embedded ASP.NET Core sample/BoldBI.Embedded.Sample/Controllers/SampleController.cs
@@ -0,0 +1,106 @@
+﻿namespace SampleCoreApp.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Net.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Newtonsoft.Json;
+    using SampleCoreApp.Models;
+
+    public class SampleController : Controller
+    {
+        private static IConfiguration configuration;
+
+        public SampleController(IConfiguration iConfig)
+        {
+            configuration = iConfig;
+        }
+
+        public IActionResult DefaultDashboard()
+        {
+            ViewBag.CurrentPage = "Dashboard";
+            ViewBag.CurrentUrl = HttpContext.Request.Path.ToString();
+            ViewBag.ServerUrl = configuration.GetSection("AppSettings:ServerURL").Value;
+           return View("sample");
+        }
+
+        [Route("/dashboard/", Name = "defaultDashJS")]
+        public IActionResult Dashboard()
+        {
+            ViewBag.CurrentPage = "Dashboard";
+            ViewBag.CurrentUrl = HttpContext.Request.Path.ToString();
+            ViewBag.ServerUrl = configuration.GetSection("AppSettings:ServerURL").Value;
+            return View("sample");
+        }
+
+        [HttpPost]
+        public string GetEmbedDetails([FromBody]object embedQuery)
+        {
+            var embedClass = Newtonsoft.Json.JsonConvert.DeserializeObject<EmbedDetail>(embedQuery.ToString());
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+            var userEmail = HttpContext.Request.Cookies["userEmailEmbeddedBI"];
+            userEmail = string.IsNullOrWhiteSpace(userEmail) ? configuration.GetSection("AppSettings:UserEmail").Value : userEmail;
+            embedClass.embedQuerString += "&embed_user_email=" + userEmail;
+            var embedDetailsUrl = "/embed/authorize?" + embedClass.embedQuerString.ToLower() + "&embed_signature=" + GetSignatureUrl(embedClass.embedQuerString.ToLower());
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(embedClass.dashboardServerApiUrl);
+                    client.DefaultRequestHeaders.Accept.Clear();
+
+                    var result = client.GetAsync(embedClass.dashboardServerApiUrl + embedDetailsUrl).Result;
+                    string resultContent = result.Content.ReadAsStringAsync().Result;
+                    return resultContent;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public Token GetToken()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51778/bi");
+                client.DefaultRequestHeaders.Accept.Clear();
+
+                var content = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("grant_type", "embed_secret"),
+                    new KeyValuePair<string, string>("Username", configuration.GetSection("AppSettings:UserEmail").Value),
+                    new KeyValuePair<string, string>("Embed_Secret", configuration.GetSection("AppSettings:EmbedSecret").Value)
+                });
+                var result = client.PostAsync(configuration.GetSection("AppSettings:RootURL").Value + "/api/" + configuration.GetSection("AppSettings:SiteIdentifier").Value + "/token", content).Result;
+                string resultContent = result.Content.ReadAsStringAsync().Result;
+                var response = JsonConvert.DeserializeObject<Token>(resultContent);//Token token = new Token();
+                var tokenObj = response; //JsonConvert.DeserializeObject<Token>(response.Token);
+                return tokenObj;
+            }
+        }
+
+
+        public string GetSignatureUrl(string message)
+        {
+            var encoding = new System.Text.UTF8Encoding();
+            var keyBytes = encoding.GetBytes(configuration.GetSection("AppSettings:EmbedSecret").Value);  //encoding.GetBytes("zjgRSUERGI6m9zvfEgHTT1bQ4FwDeszr"); //encoding.GetBytes("8ghp9g1yPLyOPycV3V7WlI51LmEFjBj9"); //encoding.GetBytes("pUu8DKaujKIyXdYAPWgvj1jW2OsCXuq");
+            var messageBytes = encoding.GetBytes(message);
+            using (var hmacsha1 = new System.Security.Cryptography.HMACSHA256(keyBytes))
+            {
+                var hashMessage = hmacsha1.ComputeHash(messageBytes);
+                return Convert.ToBase64String(hashMessage);
+            }
+        }
+
+        static double DateTimeToUnixTimeStamp(DateTime dateTime)
+        {
+            DateTime unixStart = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            long unixTimeStampInTicks = (dateTime.ToUniversalTime() - unixStart).Ticks;
+            return unixTimeStampInTicks / TimeSpan.TicksPerSecond;
+        }
+    }
+}
---
+++ Scenario Based Samples/Quick Bold BI Embedded ASP.NET Core sample/BoldBI.Embedded.Sample/wwwroot/js/site.js
@@ -0,0 +1,161 @@
+﻿var query = "";
+var queryList = [];
+window.IsAutoRefreshEnabled = false;
+window.IsEventEnabled = true;
+
+$(document).ready(function () {
+    var dropdown = document.getElementsByClassName("dropdown-btn");
+    var i;
+
+    for (i = 0; i < dropdown.length; i++) {
+        dropdown[i].addEventListener("click", function () {
+            var dropdownContent = this.nextElementSibling;
+            if (dropdownContent.style.display === "block") {
+                dropdownContent.style.display = "none";
+                $(this.firstChild.nextElementSibling.firstChild).removeClass("fa-chevron-down");
+                $(this.firstChild.nextElementSibling.firstChild).addClass("fa-chevron-right");
+            } else {
+                dropdownContent.style.display = "block";
+                $(this.firstChild.nextElementSibling.firstChild).removeClass("fa-chevron-right");
+                $(this.firstChild.nextElementSibling.firstChild).addClass("fa-chevron-down");
+            }
+        });
+    }  
+        renderDashboard(window.DashboardPath);
+
+});
+
+$(document).on("click", "#tab-properties", function () {
+    $("#property-container").addClass("rows");
+    $("#dashboard-property").removeClass("display-none");
+    
+        var dashboardViewerInstance = $("#dashboard-frame-container_embeddedbi").data("ejDashboardDesigner");
+        dashboardViewerInstance.resizeDashboard();
+
+});
+
+$(document).on("click", "#property-close", function () {
+    $("#property-container").removeClass("rows");
+    $("#dashboard-property").addClass("display-none");
+        var dashboardViewerInstance = $("#dashboard-frame-container_embeddedbi").data("ejDashboardDesigner");
+        dashboardViewerInstance.resizeDashboard();
+});
+
+$(document).on("click", ".header .header-left-side .navbar-nav", function () {
+    var dashboardViewerInstance = $("#dashboard-frame-container_embeddedbi").data("ejDashboardDesigner");
+    dashboardViewerInstance.resizeDashboard();
+    if (!$("#body-container #dashboard-property .dashboard-text-field ul").hasClass("show-extend")) {
+        $("#body-container #dashboard-property .dashboard-text-field ul").addClass("show-extend");
+    } else {
+        $("#body-container #dashboard-property .dashboard-text-field ul").removeClass("show-extend");
+    }
+});
+
+
+$(document).on("click", "#dashboard-property .load-dashboard-button", function () {
+
+        var path = $("#dashboard-property .dashboard-text-field ul li.active").attr("data-path");
+        if (typeof (path) != "undefined") {
+            renderDashboard(path);
+        }
+});
+
+
+$(document).on("click", "#dashboard-property .dashboard-text-field ul li", function (e) {
+    if (!$(this).hasClass("disabled")) {
+        $("#dashboard-property .dashboard-text-field ul li").removeClass("active");
+        $(this).addClass("active");
+            $("#select-dashboard span").html($(this).text());
+            $("#dashboard-id").val($(this).attr("data-id"));
+            $("#dashboard-path").val($(this).attr("data-path"));
+
+    }
+});
+
+function renderDashboard(path) {
+    var dashboard = BoldBI.create({
+        serverUrl: window.ServiceUrl,
+        dashboardPath: path.charAt(0) == "/" ? path : ("/" + path),
+        embedContainerId: "dashboard-frame-container",
+        embedType: BoldBI.EmbedType.Component,
+        environment: BoldBI.Environment.Cloud,
+        width: "100%",
+        height: "700px",
+        expirationTime: 100000,
+        authorizationServer: {
+            url: window.AuthorizeAPI
+        },
+        actionBegin: "renderActionBegin"
+    });
+
+    dashboard.loadDashboard();
+}
+
+
+function renderActionComplete(arg) {
+    if (arg.eventType == "renderWidget" && window.IsAutoRefreshEnabled) {
+        addEventLog("Widget is refreshed");
+    }
+}
+
+function addEventLog(eventLog) {
+    var event = $("#eventTraceElement");
+    event.append("<p>" + eventLog + "</p>");
+    $("#eventTraceElement").animate({ scrollTop: $("#eventTraceElement p").length * 27 }, "fast");
+};
+
+function addRenderingEventLog(eventLog) {
+    if (window.IsEventEnabled) {
+        var event = $("#eventTraceElement");
+        event.append("<p>" + eventLog + "</p>");
+        $("#eventTraceElement").animate({ scrollTop: $("#eventTraceElement p").length * 27 }, "fast");
+    }
+};
+
+function renderEventDashboard(path) {
+    var dashboard = BoldBI.create({
+        serverUrl: window.ServiceUrl,
+        dashboardPath: path.charAt(0) == "/" ? path : ("/" + path),
+        embedContainerId: "dashboard-frame-container",
+        embedType: BoldBI.EmbedType.Component,
+        environment: window.Environment.toLowerCase() == "cloud" ? BoldBI.Environment.Cloud : BoldBI.Environment.Enterprise,
+        width: "100%",
+        height: "700px",
+        expirationTime: 100000,
+        authorizationServer: {
+            url: window.AuthorizeAPI
+        },
+        actionBegin: "renderActionBegin",
+        actionComplete: "renderEventActionComplete",
+        dashboardSettings: {
+            beforeIconRender: "beforeDashboardIconRender"
+        },
+        widgetSettings: {
+            beforeIconRender: "beforeWidgetIconRender"
+        },
+        beforeContextMenuRnder: "beforeOtherContextMenuRender",
+    });
+
+    dashboard.loadDashboard();
+}
+
+function renderActionBegin(arg) {
+    if (arg.eventType == "renderLayout") {
+        addRenderingEventLog("Dashboard layout rendering begin event");
+    }
+    else if (arg.eventType == "resizeDashboard") {
+        addRenderingEventLog("Layout resize begin event");
+    }
+    if (arg.eventType == "renderDashboard") {
+        $("#dashboard-frame-container").css({ 'border-right': '1px solid rgb(200, 200, 200)' });
+    }
+}
+
+function renderEventActionComplete(arg) {
+    if (arg.eventType == "renderWidget") {
+        addRenderingEventLog("Widget rendering complete event");
+    }
+}
+function beforeOtherContextMenuRender(arg) {
+    addRenderingEventLog("Other context menu icon is rendering event");
+}
