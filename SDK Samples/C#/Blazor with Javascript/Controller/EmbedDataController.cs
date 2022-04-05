d17e6ea07f36a7e37fd9ccc0674c21c4d8d60fed
Pavithra Kannan <pavithra.kannan@syncfusion.com>
2022-04-05 17:19:11 +0530

    Moved Blazor forms sample.

---
+++ SDK Samples/C#/Blazor with Javascript/Controller/EmbedDataController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BoldBIEmbedSample.Models;
+using Newtonsoft.Json;
+
+namespace BoldBIEmbedSample.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmbedDataController : ControllerBase
+    {
+        private readonly IWebHostEnvironment hostingEnv;
+
+        public EmbedDataController(IWebHostEnvironment env)
+        {
+            this.hostingEnv = env;
+        }
+
+        [HttpPost("[action]")]
+        [Route("GetDetails")]
+        public string GetEmbedDetails([FromBody] object embedQuerString)
+        {
+            var embedClass = JsonConvert.DeserializeObject<EmbedClass>(embedQuerString.ToString());
+            var embedQuery = embedClass.embedQuerString;
+            // User your user-email as embed_user_email
+            embedQuery += "&embed_user_email=" + EmbedProperties.UserEmail;
+            var embedDetailsUrl = "/embed/authorize?" + embedQuery + "&embed_signature=" + GetSignatureUrl(embedQuery);
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(embedClass.dashboardServerApiUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+
+                var result = client.GetAsync(embedClass.dashboardServerApiUrl + embedDetailsUrl).Result;
+                string resultContent = result.Content.ReadAsStringAsync().Result;
+                return resultContent;
+            }
+        }
+
+        public string GetSignatureUrl(string message)
+        {
+            var encoding = new System.Text.UTF8Encoding();
+            var keyBytes = encoding.GetBytes(EmbedProperties.EmbedSecret);
+            var messageBytes = encoding.GetBytes(message);
+            using (var hmacsha1 = new System.Security.Cryptography.HMACSHA256(keyBytes))
+            {
+                var hashMessage = hmacsha1.ComputeHash(messageBytes);
+                return Convert.ToBase64String(hashMessage);
+            }
+        }
+    }
+}
+
---
+++ SDK Samples/C#/Blazor with Javascript/Models/EmbedClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace BoldBIEmbedSample.Models
+{
+    public class EmbedClass
+    {
+        [DataMember]
+        public string embedQuerString { get; set; }
+        [DataMember]
+        public string dashboardServerApiUrl { get; set; }
+    }
+}
