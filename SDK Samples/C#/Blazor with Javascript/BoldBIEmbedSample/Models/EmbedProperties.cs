97d95232032a915b9a430a3d9612cccc4a2edbdc
Pavithra Kannan <pavithra.kannan@syncfusion.com>
2022-04-13 10:32:51 +0530

    Included Blazor sample into Github.

---
+++ SDK Samples/C#/Blazor with Javascript/BoldBIEmbedSample/Models/EmbedProperties.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoldBIEmbedSample.Models
+{
+    public class EmbedProperties
+    {
+        //Enter your BoldBI credentials here.
+        public static string UserEmail = ""; //Provide the User Email
+        public static string UserPassword = ""; //Provide the User Password
+
+
+        // Get the embedSecret key from Bold BI.
+        public static string EmbedSecret = ""; //Provide the embed secret key.
+    }
+}
