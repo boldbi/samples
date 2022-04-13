2dfb5b8877546737010b26bfd736cb426e8a9213
Pavithra Kannan <pavithra.kannan@syncfusion.com>
2022-04-13 11:09:59 +0530

    Included ASP.NET sample into Github.

---
+++ SDK Samples/C#/ASP.NET/EmbedSampleApplication/Default.aspx.cs
@@ -0,0 +1,58 @@
+﻿using EmbedSampleApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+
+using System.Web.UI.WebControls;
+
+namespace EmbedSampleApplication
+{
+    public partial class _Default : Page
+    {
+        [WebMethod()]
+        public static void GetEmbedDetails(string embedQuerString, string dashboardServerApiUrl)
+        {
+            embedQuerString += "&embed_user_email=" + EmbedProperties.UserEmail;
+            var embedDetailsUrl = "/embed/authorize?" + embedQuerString.ToLower() + "&embed_signature=" + GetSignatureUrl(embedQuerString.ToLower());
+
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                client.BaseAddress = new Uri(dashboardServerApiUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+
+                var result = client.GetAsync(dashboardServerApiUrl + embedDetailsUrl).Result;
+                string resultContent = result.Content.ReadAsStringAsync().Result;
+                //return resultContent;
+                var js = new System.Web.Script.Serialization.JavaScriptSerializer();
+
+                HttpContext.Current.Response.Clear();
+                HttpContext.Current.Response.ContentType = "application/json; charset=utf-8";
+                HttpContext.Current.Response.Write(js.Serialize(resultContent));
+                HttpContext.Current.Response.Flush();
+                HttpContext.Current.Response.End();
+            }
+        }
+
+        public static string GetSignatureUrl(string message)
+        {
+            var encoding = new System.Text.UTF8Encoding();
+            var keyBytes = encoding.GetBytes(EmbedProperties.EmbedSecret);
+            var messageBytes = encoding.GetBytes(message);
+            using (var hmacsha1 = new HMACSHA256(keyBytes))
+            {
+                var hashMessage = hmacsha1.ComputeHash(messageBytes);
+                return Convert.ToBase64String(hashMessage);
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ SDK Samples/C#/ASP.NET/README.md
@@ -0,0 +1,37 @@
+# Run the application with embedded dashboard in three steps
+
+1. Download the sample application and open the solution file in the Visual Studio IDE. 
+2. You need to set your embed property details in the EmbedProperties.cs and Site.Master as follows.
+
+<meta charset="utf-8"/>
+<table>
+  <tbody>
+    <tr>
+        <td align="left">UserEmail</td>
+        <td align="left">The email of the user. The dashboard will be rendered on behalf of this user.</td>
+    </tr>
+    <tr>
+        <td align="left">EmbedSecret</td>
+        <td align="left">You can get your EmbedSecret key from the Embed tab by enabling `Enable embed authentication` in Administration page https://help.boldbi.com/embedded-bi/site-administration/embed-settings/.</td>
+    </tr>
+    <tr>
+        <td align="left">RootUrl</td>
+        <td align="left">Dashboard Server URL (Eg: http://localhost:5000/bi, http://demo.boldbi.com/bi)</td>
+    </tr>
+    <tr>
+        <td align="left">SiteIdentifier</td>
+        <td align="left">For Bold BI On-Premise Edition, it should be like `site/site1`. For Bold BI Cloud, it should be an empty string.</td>
+    </tr>
+    <tr>
+        <td align="left">Environment</td>
+        <td align="left">Your Bold BI application environment. (If Cloud, use `cloud`; if On-Premise, use `enterprise`)</td>
+    </tr>
+        <tr>
+        <td align="left">DashboardId</td>
+        <td align="left">Item id of the dashboard to be embedded in your application.</td>
+    </tr>
+  </tbody>
+</table>
+ 
+ 3. Then, run the ASP.NET sample.
+                                                           
