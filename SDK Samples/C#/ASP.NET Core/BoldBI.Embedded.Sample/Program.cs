e657b63a9f5e83b71a4271828495ab520c98466c
nithyagopal123 <91586758+nithyagopal123@users.noreply.github.com>
2023-04-11 17:11:49 +0530

    UMP-23843-Migrate-to-ASPNET-Core-2.1-Projects-to-ASPDOTNET-6

--- SDK Samples/C#/ASP.NET Core/BoldBI.Embedded.Sample/Program.cs
+++ SDK Samples/C#/ASP.NET Core/BoldBI.Embedded.Sample/Program.cs
@@ -1,24 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+﻿using BoldBI.Embedded.Sample.Models;
+using Newtonsoft.Json;
 
-namespace BoldBI.Embedded.Sample
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+string BasePath = AppDomain.CurrentDomain.BaseDirectory;
+
+// Read the contents of embedconfig.json
+string jsonString = File.ReadAllText(BasePath + "\\app_data\\embedConfig.json");
+
+// Deserialize the JSON string into a GloblaAppSettings.
+GlobalAppSettings.EmbedDetails = JsonConvert.DeserializeObject<EmbedDetails>(jsonString);
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-    }
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
