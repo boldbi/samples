0e45ed091beb77f7c94854a4cfa0f0bb8620f6d7
anuabarnab4 <anuabarna.sf4019@gmail.com>
2024-03-12 12:02:17 +0530

    updated iframe solution sample

---
+++ Scenario Based Samples/Iframe Based Dashboard Listing/IframeSolutionSample/Models/Token.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+namespace IframeSolutionSample.Models
+{
+    public class Token
+    {
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("token_type")]
+        public string TokenType { get; set; }
+
+        [JsonProperty("expires_in")]
+        public string ExpiresIn { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        public string LoginResult { get; set; }
+
+        public string LoginStatusInfo { get; set; }
+
+        [JsonProperty(".issued")]
+        public string Issued { get; set; }
+
+        [JsonProperty(".expires")]
+        public string Expires { get; set; }
+    }
+}
