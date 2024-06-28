16b79281be97dcf1897a44b60b754275e9c45de0
SuryaVijayakumarSF4316 <149993554+SuryaVijayakumarSF4316@users.noreply.github.com>
2024-06-28 11:52:00 +0530

    Added the dashboard and widget comment sample.

---
+++ Scenario Based Samples/Dashboard and Widget comment/BoldBI.Embed.Sample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BoldBI.Embed.Sample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
